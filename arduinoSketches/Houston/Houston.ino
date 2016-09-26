3e13f7bb434418affdd818fb362e51bf88d93015
bfindlay <bfin5330@uni.sydney.edu.au>
2016-09-27 03:12:59 +1000

    basic functionality for control station arduino

---
+++ arduinoSketches/Houston/Houston.ino
@@ -0,0 +1,76 @@
+/**** BLUETOOTH SETUP ****/
+#include <SoftwareSerial.h>                     // Software Serial Port
+
+
+#define DEBUG_ENABLED  1
+
+
+SoftwareSerial bt(2,3);  //TX | RX
+
+/*****************************/
+
+
+void setup() {
+  
+  Serial.begin(9600);
+
+  /*** Bluetooth setup *******************************/
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);
+  bt.begin(9600);
+
+  /*****************************************************/
+ 
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+
+  //testing the methods
+  blinkAll();
+ 
+}
+
+
+
+
+
+void loop() {
+
+  //Test sending bluetooth command to slave
+
+    //Receive from server
+   if (Serial.available()) {
+    char input = Serial.read();  // read first available byte into a variable
+    // THis is where we retrieve the data from the server,
+    //Then send it to the rover via bluetooth
+     bt.write(input);
+  }
+  
+
+  /******* READ BLUETOOTH SERIAL ******/
+  if(bt.available()){
+     //THis will be where we handle the response from the rover
+      int r = bt.read();
+      //send rover response to server
+      Serial.print("bbbb");
+    }
+}
+
+
+void blinkAll(){
+  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, HIGH);
+
+  delay(1000);
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,LOW);
+
+}
+
+
+
