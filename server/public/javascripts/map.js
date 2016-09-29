98cd4278c3e8764c278f9c53977cd7b58eecffb6
bfindlay <bfin5330@uni.sydney.edu.au>
2016-09-29 16:26:23 +1000

    initial canvas sketch

---
+++ server/public/javascripts/map.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function setup() {
+  createCanvas(800, 400);
+  background(255, 0, 0);
+}
+
+// CASE 3
+// Only draw().
+// createCanvas() is called automatically with defaults.
+function draw() {
+
+}
+
+
+let plotter = (x, y) => {
+  //  console.log("got a plot", x, y);
+  let xx = Math.cos(x * Math.PI / 180) * y;   // move object 17 pixels in direction 257
+  // y += sin(257*pi/180)*17;`
+  let yy = Math.sin(x * Math.PI / 180) * y;
+
+  xx += 400;
+  yy += 200;
+
+  console.log(xx, yy);
+  ellipse(xx, yy, 10, 10);
+};
