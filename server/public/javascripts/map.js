c48564d84c1e71144025257bff2955f8414f1294
bfindlay <bfin5330@uni.sydney.edu.au>
2016-09-30 02:01:35 +1000

    working rover plotting for forward mode

--- server/public/javascripts/map.js
+++ server/public/javascripts/map.js
@@ -8,18 +8,16 @@ function setup() {
 }
 
 function draw() {
-    //rover.show;
-    console.log("x", mouseX);
-    console.log("y", mouseY);
+    rover.show();
 }
 
 
 let plotter = (x, y) => {
   //  console.log("got a plot", x, y);
-  let xx = (Math.cos(x * Math.PI / 180) * y)+400;   // move object 17 pixels in direction 257
+  let xx = (Math.cos(x * Math.PI / 180) * y)+ rover.x;   // move object 17 pixels in direction 257
   // y += sin(257*pi/180)*17;`
-  let yy = (Math.sin(x * Math.PI / 180) * y)+200;
-  //console.log(xx, yy);
+  let yy = (Math.sin(x * Math.PI / 180) * y)+ rover.y;
+  fill(255);
   ellipse(xx, yy, 10, 10);
 };
 
@@ -36,32 +34,12 @@ function Rover(){
   };
 
   this.update = function(heading){
-  
-    if(heading < 10){
-      this.xOffset +=  0;
-      this.yOffset -= 2;
-    }else if(heading < 20){
-      this.xOffset +=  1;
-      this.yOffset -= 2;
-    } else if(heading < 25){
-      this.xOffset +=  2;
-      this.yOffset -= 3;
-    }else if(heading < 180){
-      this.xOffset += 2;
-      this.yOffset += 1;
-    }else if(heading < 180){
-       this.xOffset -=2;
-      this.yOffset +=1;
-    }else{
-      this.xOffset -=2;
-      this.yOffset -=1;
-    }
-   
-   this.x = (Math.cos(heading * Math.PI / 180) * 2) + this.xOffset ;   // move object 2 pixels in direction heading
-   this.y = (Math.sin(heading * Math.PI / 180) * 2) + this.yOffset;
 
-   this.show();
-  //console.log("it is", this.y);
+    //make the new plot in direction pointing 
+    this.x = (Math.cos(heading * Math.PI / 180) * 1.5)+ this.x;   // offset x = width/2 initially
 
+    this.y = (Math.sin(heading * Math.PI / 180) * 1.5)+ this.y;  // offset y = height/2 initially
+
+    ellipse(this.x, this.y, 10, 10); // draw the new point;
   };
 }
