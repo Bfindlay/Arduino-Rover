bb226c8a76b13508b3410088a209e21feb5c7881
bfindlay <bfin5330@uni.sydney.edu.au>
2016-09-30 16:34:23 +1000

    working ping scan

--- server/public/javascripts/map.js
+++ server/public/javascripts/map.js
@@ -1,29 +1,83 @@
 'use strict';
 
 
+let detected = [];
+
 function setup() {
-  
+
+
+  // canvas setup
   let height = $('.map-flex').height();
   let width = $('.map-flex').width();
   var canvas  = createCanvas(width, height);
   canvas.parent('mapbox');
-  background(255, 0, 0);
   window.rover = new Rover();
+  window.ping = new Ping();
+  ping.show();
   rover.show();
+
+
 }
 
 function draw() {
+    clear();
     rover.show();
+    ping.show();
+    ping.update();
+    ping.obstacles();
 }
-
 
 let plotter = (x, y) => {
   let xx = (Math.cos(x * Math.PI / 180) * y)+ rover.x;  
   let yy = (Math.sin(x * Math.PI / 180) * y)+ rover.y;
+
+  detected.push([xx,yy]);
   fill(255);
-  ellipse(xx, yy, 10, 10);
+  //ellipse(xx, yy, 10, 10);
 };
 
+function Ping(){
+    this.y = ( $('.map-flex').height() / 2);
+    this.x = ( $('.map-flex').width() / 2 );
+    this.radius = 0;
+    this.distance = () => {
+        var a =  $('.map-flex').width()/2 - this.radius;
+        var b =  $('.map-flex').height()/2 - this.radius; 
+        return Math.sqrt( a*a + b*b );
+    };
+
+    this.show = function(){
+     
+      noFill();
+      stroke(0,255,0);
+      ellipse(this.x, this.y, this.radius, this.radius);
+       ellipse(this.x, this.y, this.radius -10, this.radius-10);
+      
+      
+
+    };
+
+    this.update = function(){
+      if(this.radius > this.x * 3){
+        this.radius = 0;
+      }else{
+        this.radius +=6;
+      }
+    }
+
+    this.obstacles = function(){
+      
+      detected.forEach( e => {
+
+        var a = this.x - e[0];
+        var b = this.y - e[1];
+        var c = Math.sqrt( a*a + b*b );
+        if((this.distance() - c ) < 2){
+          ellipse(e[0],e[1], 10, 10);
+        }
+      });
+    };
+}
 
 function Rover(){
  
@@ -31,8 +85,9 @@ function Rover(){
   this.x = ( $('.map-flex').width() / 2 );
 
   this.show = function(){
-    fill(0);
-    ellipse(this.x,this.y,10,10);
+    fill(0, 255, 0);
+    noStroke();
+    ellipse(this.x,this.y,5,5);
   };
 
   this.update = function(heading){
@@ -42,6 +97,6 @@ function Rover(){
 
     this.y = (Math.sin(heading * Math.PI / 180) * 1.5)+ this.y;  // offset y = height/2 initially
 
-    ellipse(this.x, this.y, 10, 10); // draw the new point;
+    ellipse(this.x, this.y, 5, 5); // draw the new point;
   };
 }
