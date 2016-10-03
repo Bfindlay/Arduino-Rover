e9fa4ba7e9eee5812858eb72e15667dac96417de
bfindlay <bfin5330@uni.sydney.edu.au>
2016-10-04 00:23:36 +1100

    added port selection capabilities

--- server/app.js
+++ server/app.js
@@ -6,63 +6,37 @@ let path = require('path');
 let servers = require('http').Server(app);
 let io = require('socket.io')(servers);
 let home = require('./routes/home');
+let bodyParser = require('body-parser');
 let api = require('./routes/api');
-
-var SerialPort = require('serialport');
-
-var serialport = new SerialPort('/dev/cu.usbmodem14131', {
-  //parser: SerialPort.parsers.byteLength(3)
- // parser: SerialPort.parsers.readline('\n')
- //parser: SerialPort.parsers.byteLength(5)
- parser: SerialPort.parsers.byteDelimiter([123,125])
-  //parser: SerialPort.parsers.readline('\n')
-});
-
-
-
-// creating the parser and piping can be shortened to
-//var parser = port.pipe(new ReadLine());
-
+let SerialPort = require('serialport');
+let serialport = new SerialPort('null', {
+		parser: SerialPort.parsers.byteDelimiter([123,125])
+	});
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(express.static(__dirname + '/public'));
-
 app.use('/', home);
 app.use('/api', api);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
-// TODO handle undefined
-let safeParse = data => {
-	try {
-		return JSON.parse(data.toString().replace(/\s/g, '').replace(/'/g, '"'));
-	} catch (e) {
-		console.log("parse error", e);
-	}
-};
 
-
-/*** SERIAL PORT CONNECTION */
-serialport.on('error', function(err) {
-	console.log("there was an error", err)
-});
-
-serialport.on('open', function() {
-
-	//  TODO handle on disconnect event
-
+let connect = port => {
+	console.log("connecting");
+	serialport = new SerialPort(port, {
+		parser: SerialPort.parsers.byteDelimiter([123,125])
+	});
+	serialport.on('open', function() {
 	// Now server is connected to Arduino
-	console.log('Serial Port Opend');
-	io.sockets.on('connection', function(socket) {
+	console.log('Serial Port open');
+	io.sockets.on('connection', socket => {
 		//  Connecting to client 
 		console.log('Socket connected');
 		socket.emit('connected');
 
-		serialport.on('data', function(data) {
-			//   console.log("Returned", data);
-
+		serialport.on('data', data => {
 			let result = safeParse(new Buffer(data));
-			//  TODO fix this lol
 			//  TODO handle when result is undefined
 			console.log(result);
 			if (result !== undefined) {
@@ -76,15 +50,47 @@ serialport.on('open', function() {
 				}
 			}
 		});
-	
-		socket.on('data', function(data) {
-			// FOR DEBUGGING console.log("got data", data);
+		serialport.on('error', err =>{
+			console.log(err);
+		});
+		socket.on('data', data => {
+			console.log("data" + data);
 			serialport.write(data);
 		});
+		process.on('uncaughtException', err =>{
+			console.log(err);
+		})
 	});
-
 });
+};
+
+//TODO handle undefined
+let safeParse = data => {
+	try {
+		return JSON.parse(data.toString().replace(/\s/g, '').replace(/'/g, '"'));
+	} catch (e) {
+		console.log("parse error", e);
+	}
+};
+
+
+/*** SERIAL PORT CONNECTION */
+serialport.on('error', function(err) {
+	console.log("there was an error", err);
+	io.sockets.on('connection', function(socket){
+		SerialPort.list(function (err, ports) {
+			let availPorts = ports.filter( p =>  p.comName !== undefined);
+			socket.emit('ports', availPorts);
+		});
+	});
+});
+
+
 
 servers.listen(process.env.port || 3000, function() {
 	console.log("running");
+});
+
+app.post('/port', function(req, res){
+	connect(req.body.data);
 });
