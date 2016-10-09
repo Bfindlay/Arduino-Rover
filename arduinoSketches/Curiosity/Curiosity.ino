8e8276ea33819bbe6038ba24f17c92f85377cce4
bfindlay <bfin5330@uni.sydney.edu.au>
2016-10-10 01:21:41 +1100

    basic auto/manual switching

--- arduinoSketches/Curiosity/Curiosity.ino
+++ arduinoSketches/Curiosity/Curiosity.ino
@@ -1,14 +1,14 @@
 #include <SoftwareSerial.h>                     // Software Serial Port
-#include <Servo.h>  
-#include "Delay.h" 
+#include <Servo.h>
+#include "Delay.h"
 //A4 -> SDA
 //A5 ->SCL
-NonBlockDelay d; 
+NonBlockDelay d;
 NonBlockDelay search;
 NonBlockDelay inner;
-SoftwareSerial bt(2,3);  //TX | RX
+SoftwareSerial bt(2, 3); //TX | RX
 
-/**** SERVO ****/               
+/**** SERVO ****/
 Servo servoRight;
 Servo servoLeft;
 Servo scanner;
@@ -32,50 +32,53 @@ int dist;
 int distLeft;
 int distRight;
 bool avoiding = false;
+bool Auto = true;
 void setup() {
   //Serial.begin(9600);
   /* Initialise the sensor */
-  if(!mag.begin())
+  if (!mag.begin())
   {
     /* There was a problem detecting the HMC5883 ... check your connections */
     //Serial.println("Ooops, no HMC5883 detected");
-    while(1);
+    while (1);
   }
-  
+
   pinMode(2, INPUT); //RX
   pinMode(3, OUTPUT); //TX
   bt.begin(57600); // Default communication rate of the Bluetooth module
-  
+
   //Trigger and echo pins for the ultrasonic sensor
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(A0, INPUT);
 
   // Add servos to pins 12 and 13
-  servoRight.attach(12); 
+  servoRight.attach(12);
   servoLeft.attach(13);
   scanner.attach(10);
   scanner.write(90);
 
-  
- 
+  pinMode(7, OUTPUT);
+  digitalWrite(7, HIGH);
+
 }
 void loop() {
 
-  if (d.Timeout()) {  
-    Move();
-    d.Delay(100);
-    //THis runs every 0.5 seconds
-    } 
+  if (Auto) {
+    if (d.Timeout()) {
+      Move();
+      d.Delay(100);
+      //THis runs every 0.5 seconds
+    }
     //this runs continuously
     check();
-    readSerial();
-   // Serial.println(dir);
-  
- }
+  }
+  readSerial();
 
-void check(){
-  if (distance() > 0 && distance() < 10){
+}
+
+void check() {
+  if (distance() > 0 && distance() < 10) {
     //Serial.println("----------------------------");
     //Serial.println("Avoiding");
     avoiding = true;
@@ -83,10 +86,10 @@ void check(){
   }
 }
 
-void avoid(){
+void avoid() {
 
   //DELAY ALLOWED BECAUSE STOPPED
-  if(search.Timeout()){
+  if (search.Timeout()) {
     //Serial.println("Scanning");
     STOP();
     scanner.write(10);
@@ -104,12 +107,12 @@ void avoid(){
     delay(250);
   }
   dir = 'F';
-  
+
 }
-void readSerial(){
-   if(bt.available()){ // Checks whether data is comming from the serial port
-      c = bt.read();
-      switch (c) {
+void readSerial() {
+  if (bt.available()) { // Checks whether data is comming from the serial port
+    c = bt.read();
+    switch (c) {
       case 'F':
         forward();
         break;
@@ -128,13 +131,27 @@ void readSerial(){
       case 'D':
         distance();
         break;
+      case 'M':
+        modeSet();
+        break;
     }
   }
 }
 
+void modeSet() {
+  Auto = !Auto;
+  if (!Auto) {
+    STOP();
+    digitalWrite(7, LOW);
+  } else {
+    digitalWrite(7, HIGH);
+    dir = 'F';
+  }
 
-void Move(){
-  switch(dir) {
+}
+
+void Move() {
+  switch (dir) {
     case 'F':
       //Serial.println("forward");
       forward();
@@ -155,77 +172,78 @@ void Move(){
 }
 
 
-void left(){
+void left() {
   //Serial.println("left");
-    dir = 'L';
-    bt.println("{'direction': 'left'}");
-    servoLeft.writeMicroseconds(1300);         // Left wheel clockwise
-    servoRight.writeMicroseconds(1300);        // Right wheel clockwise
+  dir = 'L';
+  bt.println("{'direction': 'left'}");
+  servoLeft.writeMicroseconds(1300);         // Left wheel clockwise
+  servoRight.writeMicroseconds(1300);        // Right wheel clockwise
 
 }
 
-void right(){
+void right() {
   dir = 'R';
   //Serial.println("right");
-    bt.println("{'direction': 'right'}");
-    servoLeft.writeMicroseconds(1700);         // Left wheel counterclockwise
-    servoRight.writeMicroseconds(1700);        // Right wheel counterclockwise
+  bt.println("{'direction': 'right'}");
+  servoLeft.writeMicroseconds(1700);         // Left wheel counterclockwise
+  servoRight.writeMicroseconds(1700);        // Right wheel counterclockwise
 }
 
-void forward(){
-  
-  
+void forward() {
+
+
   dir = 'F';
-    
-    compass();
-    bt.println("{'direction': 'forward'}");
-    servoLeft.writeMicroseconds(1700);        
-    servoRight.writeMicroseconds(1100);
-    
+
+  compass();
+  distance();
+  bt.println("{'direction': 'forward'}");
+  servoLeft.writeMicroseconds(1700);
+  servoRight.writeMicroseconds(1100);
+
 }
 
-void reverse(){
+void reverse() {
   dir = 'B';
-    //Serial.println("reverse");
-    bt.println("{'direction': 'backwards'}");
-    servoLeft.writeMicroseconds(1300);         // Left wheel clockwise
-    servoRight.writeMicroseconds(1700);        // Right wheel counterclockwise  
+  //Serial.println("reverse");
+  bt.println("{'direction': 'backwards'}");
+  servoLeft.writeMicroseconds(1300);         // Left wheel clockwise
+  servoRight.writeMicroseconds(1700);        // Right wheel counterclockwise
 }
 
-void STOP(){
+void STOP() {
   //Serial.println("Stopped");
   dir = 'S';
   servoLeft.writeMicroseconds(1500);         // stop left
   servoRight.writeMicroseconds(1500);
 }
 
-void compass(){
-   /* Get a new sensor event */ 
-  sensors_event_t event; 
+void compass() {
+  /* Get a new sensor event */
+  sensors_event_t event;
   mag.getEvent(&event);
- 
+
   // Calculate heading when the magnetometer is level, then correct for signs of axis.
   float heading = atan2(event.magnetic.y, event.magnetic.x);
-  
+
   // Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
   // Find yours here: http://www.magnetic-declination.com/
   // Mine is: -13* 2' W, which is ~13 Degrees, or (which we need) 0.22 radians
   // If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
   float declinationAngle = 0.209;
   heading += declinationAngle;
-  
+
   // Correct for when signs are reversed.
-  if(heading < 0)
-    heading += 2*PI;
-    
+  if (heading < 0)
+    heading += 2 * PI;
+
   // Check for wrap due to addition of declination.
-  if(heading > 2*PI)
-    heading -= 2*PI;
-   
+  if (heading > 2 * PI)
+    heading -= 2 * PI;
+
   // Convert radians to degrees for readability.
-  float headingDegrees = heading * 180/M_PI; 
-  
- //Serial.print("Heading (degrees): "); Serial.println(headingDegrees);
+  float headingDegrees = heading * 180 / M_PI;
+
+  //Serial.print("Heading (degrees): "); Serial.println(headingDegrees);
   // send heading to control arduino
   bt.print("{'heading': '");
   bt.print(headingDegrees);
@@ -233,21 +251,21 @@ void compass(){
 
 }
 
-int distance(){
+int distance() {
 
   digitalWrite(trigPin, LOW);
   delayMicroseconds(5);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  
+
   // Read the signal from the sensor: a HIGH pulse whose
   // duration is the time (in microseconds) from the sending
   // of the ping to the reception of its echo off of an object.
   pinMode(echoPin, INPUT);
   duration = pulseIn(echoPin, HIGH);
   // convert the time into a distance
-  cm = (duration/2) / 29.1;
+  cm = (duration / 2) / 29.1;
   dist = cm;
   bt.print("{'distance': '");
   bt.print(cm);
